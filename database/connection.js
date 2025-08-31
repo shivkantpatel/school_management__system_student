db7ac17088457a735f4cb756b830bd96259bf073
shivkant patel <shivkantp159@gmail.com>
2025-08-31 18:07:13 +0530

    database connection change

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
-
 let connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Shivkant77@',
-    database:'school_management_system'
+
+    host: 'metro.proxy.rlwy.net',           // ✅ Correct Railway host
+    user: 'root',
+    port: 26525,                            // ✅ Correct Railway port
+    password: 'sLNcmhAoURrTLIVBfvwORoezBHfJaaWe',
+    database: 'school_management_system'
 });
+
 
 
 module.exports = connection
