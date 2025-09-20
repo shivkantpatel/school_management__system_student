d50b4f0414a94f82d5c0596cfd97428d88c29208
shivkant patel <shivkantp159@gmail.com>
2025-09-20 10:39:22 +0530

    database changes

--- database/connection.js
+++ database/connection.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql2');
 let connection = mysql.createConnection({
 
-    host: 'metro.proxy.rlwy.net',           // ✅ Correct Railway host
-    user: 'root',
-    port: 26525,                            // ✅ Correct Railway port
-    password: 'sLNcmhAoURrTLIVBfvwORoezBHfJaaWe',
+  host: 'localhost',           //
+    user: 'root',                         
+    password: 'Shivkant77@',
     database: 'school_management_system'
 });
+// let connection = mysql.createConnection({
+
+//     host: 'metro.proxy.rlwy.net',           // ✅ Correct Railway host
+//     user: 'root',
+//     port: 26525,                            // ✅ Correct Railway port
+//     password: 'sLNcmhAoURrTLIVBfvwORoezBHfJaaWe',
+//     database: 'school_management_system'
+// });
 
 
 
--- router/afterLogin/user.js
+++ router/afterLogin/user.js
@@ -3,40 +3,40 @@ const router = express();
 const conn = require('../../database/connection')
 
 
-router.get('/userDashboard',(req,res)=>{
-    
-    if (req.session.user) {
-        
-    let id = req.session.user.id;
-    
-    let query = 'select * from student_mast where id = ?';
+router.get('/userDashboard', (req, res) => {
 
-    conn.query(query,[id],(err,result)=>{
-        res.render('userDashboard/userDashboard',{result:result})
-        
-    })
-     
+    if (req.session.user) {
+
+        let id = req.session.user.id;
+
+        let query = 'select * from student_mast where id = ?';
+
+        conn.query(query, [id], (err, result) => {
+            res.render('userDashboard/userDashboard', { result: result })
+
+        })
+
 
     } else {
         res.redirect('/')
     }
-    
-    
-   
-    
+
+
+
+
 })
-router.get('/userProfile',(req,res)=>{
+router.get('/userProfile', (req, res) => {
     console.log(req.session.user);
-    
+
     if (req.session.user) {
-        
+
     } else {
         res.redirect('/')
     }
 
-    
-    
-    
+
+
+
 })
 
 
